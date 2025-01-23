6bf6ca241696e8d11b4a6dd1a29a9f512b68bcf3
Yenduri Adithya Sai <161115591+adithya-yenduri@users.noreply.github.com>
2025-01-23 15:29:41 +0530

    Add files via upload

---
+++ study.sql
@@ -0,0 +1,50 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Jan 23, 2025 at 10:54 AM
+-- Server version: 10.4.32-MariaDB
+-- PHP Version: 8.0.30
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `hello`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `study`
+--
+
+CREATE TABLE `study` (
+  `stname` varchar(10) NOT NULL,
+  `rollno` int(10) NOT NULL,
+  `age` int(10) NOT NULL,
+  `sex` tinyint(1) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `study`
+--
+
+INSERT INTO `study` (`stname`, `rollno`, `age`, `sex`) VALUES
+('sessi shek', 69, 99, 9),
+('chetAAN RI', 90, 2, 0),
+('sessi shek', 69, 99, 9),
+('ADI ', 90, 2, 0);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
